9215c137ff398dd6bdcfe75947341a8e7cb2a0e7
maksudur420 <mdmaksudur588@gmail.com>
2023-11-04 12:57:57 +0600

    commit all cart context

---
+++ src/context/CartProvider.tsx
@@ -0,0 +1,124 @@
+import { useMemo, useReducer } from "react"
+
+export type CartItemType ={
+    id:string,
+    title:string,
+    price:number,
+    category:string,
+    description:string,
+    rating:{
+        rate:number,
+        count:number
+    },
+    image:string,
+    qty:number
+}
+
+type CartStateType ={cart:CartItemType[]}
+
+const initCartState ={cart:[]}
+
+const REDUCER_ACTION_TYPE ={
+    ADD:"ADD",
+    REMOVE:"REMOVE",
+    QUANTITY:"QUANTITY",
+    SUBMIT:"SUBMIT"
+}
+
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE;
+
+export type ActionType ={
+    type:string,
+    payload?: CartItemType
+}
+
+const reducer =(state:CartStateType,action:ActionType):CartStateType=>{
+    switch(action.type){
+        case REDUCER_ACTION_TYPE.ADD:
+            {
+               if (!action.payload) {
+                throw new Error('Action.payload is missing in ADD action')
+               } 
+
+               const {id,title,price,rating,description,category,image} = action.payload
+
+               const filterCart :CartItemType[] = state.cart.filter(item=>item.id!==id)
+
+               const itemExist:CartItemType | undefined = state.cart.find(item=>item.id===id)
+
+               const qty: number = itemExist? itemExist.qty +1 : 1;
+               return {...state, cart : [...filterCart,{id,title,price,rating,description,category,qty,image}]}
+            }
+        case REDUCER_ACTION_TYPE.REMOVE:
+            {
+                if (!action.payload) {
+                    throw new Error('Action.payload is missing in Remove action')
+                   } 
+                const {id} = action.payload
+
+                const filterCart :CartItemType[] = state.cart.filter(item=>item.id!==id)
+
+                return {...state, cart : [...filterCart]}
+            }
+        case REDUCER_ACTION_TYPE.QUANTITY:
+            {
+                if (!action.payload) {
+                    throw new Error('Action.payload is missing in Quantity action')
+                } 
+                const {id,qty} = action.payload
+
+               const itemExist:CartItemType | undefined = state.cart.find(item=>item.id===id)
+
+               if (!itemExist) {
+                throw new Error('The Item is not Exist')
+               }
+               const updatedItem:CartItemType ={...itemExist,qty}
+
+               const filterCart :CartItemType[] = state.cart.filter(item=>item.id!==id)
+
+               return {...state, cart:[...filterCart,updatedItem]}
+            }
+        case REDUCER_ACTION_TYPE.SUBMIT:
+            {
+                return{...state, cart: []}
+            }
+        default:
+            throw new Error('Undefined Reducer Action type')
+            
+    }
+}
+
+const useCartContext =(initCartState:CartStateType)=>{
+    const [state,dispatch]=useReducer(reducer,initCartState)
+
+    const REDUCER_ACTIONS =useMemo(()=>{
+        return REDUCER_ACTION_TYPE;
+    },[])
+
+    const totalItems = state.cart.reduce((prev,Item)=>{
+        return prev + Item.qty
+    },0)
+
+    const TotalPrice = new Intl.NumberFormat('en-US',{style:'currency',currency:'USD'}).format(
+        state.cart.reduce((Prev,Item)=>{
+            return Prev + (Item.qty * Item.price)
+        },0)
+    )
+
+    const cart = state.cart.sort((a,b)=>{
+        const ItemA = Number(a.id)
+        const ItemB =Number(b.id)
+        return ItemA - ItemB
+    })
+
+    return {
+        dispatch,
+        REDUCER_ACTIONS,
+        totalItems,
+        TotalPrice,
+        cart
+    }
+
+}
+
+export type UseCartContextType = ReturnType < typeof useCartContext>
--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,28 +1,36 @@
-import { ReactElement, createContext, useState } from "react"
+import { ReactElement, createContext, useEffect, useState } from "react"
+
 
 export type ProductsType ={
-    sku:string,
-    name:string,
-    price:number
+    id:string,
+    title:string,
+    price:number,
+    category:string,
+    description:string,
+    rating:{
+        rate:number,
+        count:number
+    },
+    image:string
 }
 
-const initState: ProductsType[]=[
-    {
-        "sku": "item0001",
-        "name": "Widget",
-        "price": 9.99
-    },
-    {
-        "sku": "item0002",
-        "name": "Premium Widget",
-        "price": 19.99
-    },
-    {
-        "sku": "item0003",
-        "name": "Deluxe Widget",
-        "price": 29.99
-    }
-]
+// const initState: ProductsType[]=[
+//     {
+//         "sku": "item0001",
+//         "name": "Widget",
+//         "price": 9.99
+//     },
+//     {
+//         "sku": "item0002",
+//         "name": "Premium Widget",
+//         "price": 19.99
+//     },
+//     {
+//         "sku": "item0003",
+//         "name": "Deluxe Widget",
+//         "price": 29.99
+//     }
+// ]
 
 export type ProductsContextType ={
     products: ProductsType[]
@@ -39,7 +47,22 @@ type ChildrenType ={
 }
 
 export const ProductsProvider =({children}:ChildrenType):ReactElement=>{
-    const [products,setProducts] =useState<ProductsType[]>(initState)
+    const [products,setProducts] =useState<ProductsType[]>([])
+
+   
+    useEffect(()=>{
+        const fetchData = async ():Promise<ProductsType[]>=>{
+            const data = await fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .catch(err=>{
+                if (err instanceof Error) {
+                    console.log(err.message) 
+                }
+            })
+            return data
+        }
+        fetchData().then(data=>setProducts(data))
+    },[])
 
     return(
         <ProductsContext.Provider value={{products}}>
@@ -47,3 +70,4 @@ export const ProductsProvider =({children}:ChildrenType):ReactElement=>{
         </ProductsContext.Provider>
     )
 }
+export default ProductsContext;
